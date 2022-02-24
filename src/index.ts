f526325b9dc00502bf29d37a456370bede38bd9f
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-24 08:34:27 +0300

    config updates

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
-interface Food {
-  ugali: number;
-  mboga: number;
-  nyama: number;
-}
-
-const food: Food = {
-  ugali: 10,
-  mboga: 4,
-  nyama: 8,
-};
-
-console.log(food);
+export * from './events/BaseListener';
+export * from './events/user-created-event';
