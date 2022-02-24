8c27afd4ffcf4f42159b3f7fc5e583f4c0724af0
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-24 06:55:39 +0300

    config updates

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 interface Food {
-    ugali: number,
-    mboga: number;
-    nyama: number;
+  ugali: number;
+  mboga: number;
+  nyama: number;
 }
 
 const food: Food = {
-    ugali: 10,
-    mboga: 4,
-    nyama: 8
-}
+  ugali: 10,
+  mboga: 4,
+  nyama: 8,
+};
 
 console.log(food);
