47ed67a17d7175c240e058d3db4009a7065f0e06
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-27 13:55:37 +0300

    config updates

--- src/events/section-created-event.ts
+++ src/events/section-created-event.ts
@@ -1,10 +1,10 @@
 import { Subjects } from "./Subjects";
 
 export interface SectionCreatedEvent {
-    subject: Subjects.SectionCreated,
-    data: {
-        id: string; 
-        title: string;
-        createdBy: string;
-    }
+  subject: Subjects.SectionCreated;
+  data: {
+    id: string;
+    title: string;
+    createdBy: string;
+  };
 }
