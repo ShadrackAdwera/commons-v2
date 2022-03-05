81e041884049a4c501d38082f4540b9ac4629d7b
ShadrackAdwera <adweshshaddie@gmail.com>
2022-03-05 11:56:56 +0300

    update user created event

--- src/events/user-created-event.ts
+++ src/events/user-created-event.ts
@@ -4,10 +4,7 @@ export interface UserCreatedEvent {
     subject: Subjects.UserCreated,
     data: {
         id: string;
-        username: string;
         email: string;
-        company: string;
         category: string;
-        roles: string[]
     }
 }
